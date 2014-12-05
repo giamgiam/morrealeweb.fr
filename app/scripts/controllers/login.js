3e8e58e8a846bb2d4d09f5e40fc1d4e0b2d4aaec
Giampiero Morreale <giam.morreale@gmail.com>
2014-12-05 01:45:14 +0100

    login and createuser working and need lot of improuvements

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -10,49 +10,54 @@
 angular.module("morrealeWebApp")
   .controller("LoginCtrl", ["$scope", "Auth", LoginCtrl]);
 
-  function LoginCtrl ( $scope, Auth ) {
-    // $scope.auth = Auth;
-    // $scope.user = $scope.auth.$getAuth();
-    console.log(Auth)
-    // Auth.
-
-    $scope.error = {
-      display: false,
-      message: null
-    }
+  function LoginCtrl($scope, Auth) {
+    $scope.auth = Auth;
+    $scope.user = $scope.auth.$getAuth();
 
     $scope.validateLogin = function () {
-      var email = $scope.email,
-          password = $scope.password
-
-        if(email != undefined && email.length ) {
-          $scope.emailEmpty = false
-        } else {
-          $scope.emailEmpty = true
-        }
-
-        if(password != undefined && password.length ) {
-          $scope.passwordEmpty = false
-        } else {
-          $scope.passwordEmpty = true
-        }
-
-        if(email != undefined && email.length &&
-          password != undefined && password.length) {
-            Auth.$authWithPassword({
-              email: email,
-              password: password
-            }).then( function(authData) {
-              console.log("Logged in as: ", authData.uid, authData)
-            }).catch(function(error) {
-              console.log("Authentication failed: ", error)
-              if(error.code === "INVALID_USER") {
-                $scope.error = {
-                  display: true,
-                  message: error.message
-                }
-              }
-            })
+      if($scope.login.$dirty && $scope.login.userEmail.$valid && $scope.login.userPassword.$valid ) {
+        Auth.$authWithPassword({
+          email: $scope.login.userEmail.$viewValue,
+          password: $scope.login.userPassword.$viewValue
+        }).then( function(authData) {
+          console.log("Logged in as: ", authData.uid, authData)
+        }).catch(function(error) {
+          console.log("Authentication failed: ", error)
+          $scope.login.error = {
+            message: error.message
           }
-    };
-  }
+        })
+      }
+    }
+
+    $scope.createAccount = function() {
+      console.log($scope.createNewUser)
+      if($scope.createNewUser.createPassword.$viewValue != $scope.createNewUser.verifyPassword.$viewValue) {
+        $scope.createNewUser.error = {
+          message: "Password don't match"
+        }
+      }
+
+      if($scope.createNewUser.$dirty && 
+        $scope.createNewUser.createEmail.$valid &&
+        $scope.createNewUser.createPassword.$valid &&
+        $scope.createNewUser.verifyPassword.$valid) {
+        Auth.$createUser($scope.createNewUser.createEmail.$viewValue, $scope.createNewUser.createPassword.$viewValue)
+          .then(function() {
+          return Auth.$authWithPassword({
+            email: $scope.createNewUser.createEmail.$viewValue,
+            password: $scope.createNewUser.createPassword.$viewValue
+          })
+        }).then( function(authData) {
+          console.log("Logged in as: ", authData.uid, authData)
+        }).catch(function(error) {
+          console.log("Authentication failed: ", error)
+          if(error.code === "INVALID_USER") {
+            $scope.createNewUser.error = {
+              message: error.message
+            }
+          }
+        })
+      }
+    }
+   }
